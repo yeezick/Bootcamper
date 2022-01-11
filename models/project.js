6db1232409a81f3014f95b77d8d159bab10fffb7
Nicholas Rynearson <nrynearson@gmail.com>
2022-01-11 16:47:19 -0500

    sets up Project schema

---
+++ models/project.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const Project = new Schema(
+  {
+    description: {type: String, required: true, maxlength: 300},
+    owner: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    seeking: {type: Boolean, required: true},
+    team_members: [{type: Schema.Types.ObjectId, ref: "User"}],
+    title: {type: String, required: true, maxlength: 45},
+    interested_applicants: [{type: Schema.Types.ObjectId, ref: "User"}]
+
+  },
+  { timestamps: true }
+
+)
+
+export default mongoose.model("projects", Project)
