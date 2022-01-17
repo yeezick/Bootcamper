f0d8da2935cb489f3defc90708431fdfd4071728
Nicholas Rynearson <nrynearson@gmail.com>
2022-01-17 16:09:12 -0500

    revises App.js to conform to react-router-dom v6 syntax for Routes; sets up skeleton for each screen and imports into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { EditProfile } from "./screens/EditProfile/EditProfile.jsx";
+import { EditProject } from "./screens/EditProject/EditProject.jsx";
+import { Landing } from "./screens/Landing/Landing.jsx";
+import { Roulette } from "./screens/Roulette/Roulette.jsx";
+import { SignIn } from "./screens/SignIn/SignIn.jsx";
+import { SignUp } from "./screens/SignUp/SignUp.jsx";
+import { SingleProject } from './screens/SingleProject/SingleProject.jsx';
+import { UserProfile } from "./screens/UserProfile/UserProfile.jsx"
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          Landing
-        </Route>
-        <Route path="/sign-up">
-          SignUp
-        </Route>
-        <Route path="/sign-in">
-          SignIn
-        </Route>
-        <Route exact path="/users/:id/edit">
-          EditProfile
-        </Route>
-        <Route exact path="/users/:id">
-          UserProfile
-        </Route>
-        <Route exact path="/projects/:id">
-          SingleProject
-        </Route>
-        <Route exact path="/projects/:id/edit">
-          EditProject/CreateProject
-        </Route>
-        <Route path="/roulette">
-          Roulette
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/projects/:id" element={<SingleProject />} />
+        <Route exact path="/projects/:id/edit" element={<EditProject />} />
+        <Route path="/roulette" element={<Roulette />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route exact path="/users/:id" element={<UserProfile />} />
+        <Route exact path="/users/:id/edit" element={<EditProfile />} />
+      </Routes>
     </div>
   );
 }
