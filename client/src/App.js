5f73607d06f81bc565886538857b447f98d2c182
Nicholas Rynearson <nrynearson@gmail.com>
2022-01-17 15:27:07 -0500

    wraps app in Router in index.js file in react app; creates Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          Landing
+        </Route>
+        <Route path="/sign-up">
+          SignUp
+        </Route>
+        <Route path="/sign-in">
+          SignIn
+        </Route>
+        <Route exact path="/users/:id/edit">
+          EditProfile
+        </Route>
+        <Route exact path="/users/:id">
+          UserProfile
+        </Route>
+        <Route exact path="/projects/:id">
+          SingleProject
+        </Route>
+        <Route exact path="/projects/:id/edit">
+          EditProject/CreateProject
+        </Route>
+        <Route path="/roulette">
+          Roulette
+        </Route>
+      </Switch>
     </div>
   );
 }
