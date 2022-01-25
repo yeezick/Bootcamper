1fe95d7578f07a25c9fd3140ab5b05166aa294a4
Nicholas Rynearson <nrynearson@gmail.com>
2022-01-25 13:56:30 -0500

    updates SignUp screen and Form component; takes form logic out of SignUp into Form

---
+++ client/src/components/Form/Form.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Form = ({onSubmit, inputs, buttonText}) => {
+  return (
+  <form onSubmit={onSubmit}>
+    
+    {
+      inputs.map(input => (
+        <div key={input.name} className="input">
+        <label htmlFor={input.htmlFor}>{input.labelText}</label>
+        <input 
+          type={input.type}
+          name={input.name}
+          value={input.value}
+          onChange={input.onChange}
+        />
+        </div>
+      ))
+    }
+    <button type="submit">{buttonText}</button>
+  </form>
+  )
+};
--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Header } from '../../components/Header/Header.jsx'
+import { Form } from '../../components/Form/Form.jsx';
 import { signUp } from "../../services/api/users.js";
 
 export const SignUp = () => {
   const navigate = useNavigate();
-  const headerTitle = 'Register'
-  const headerText = "In order to find projects and connect with other users, we'll need to create an account."
+
+  // define state variables
   const [formData, setFormData] = useState({
     email: "",
     first_name: "",
     last_name: "",
     password: "",
-    passwordConfirm: "",
+    confirmPassword: "",
   });
+  const { first_name, last_name, email, password, confirmPassword } = formData;
 
+  // pass header information for Header component
+  const headerTitle = 'Register'
+  const headerText = "In order to find projects and connect with other users, we'll need to create an account."
+  
+ // define functions for screen
   const handleChange = (event) => {
     setFormData({
       ...formData,
@@ -23,7 +30,7 @@ export const SignUp = () => {
     });
   };
 
-  const onSignUp = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       const newUser = await signUp(formData);
@@ -33,49 +40,23 @@ export const SignUp = () => {
       console.error(error);
     }
   }
-  const { first_name, last_name, email, password, passwordConfirm } = formData;
+  
+  // define inputs array for Form component; pass both input and label information here; also pass text for submit button in Form component
+  const inputs = [
+    {type: "text", name: "first_name", value: first_name, onChange: handleChange, htmlFor:"first_name", labelText: "What's your first name?" }, 
+    {type: "text", name: "last_name", value: last_name, onChange: handleChange, htmlFor: "last_name", labelText: "What's your last name?" }, 
+    {type: "email", name: "email", value: email, onChange: handleChange, htmlFor: "email", labelText: "What's your email?" }, 
+    {type: "password", name: "password", value: password, onChange: handleChange, htmlFor: "password", labelText: "Create a password:" },
+    {type: "password", name: "confirmPassword", value: confirmPassword, onChange: handleChange, htmlFor: "confirmPassword", labelText: "Confirm password:" },
+  ]
+  const buttonText = "Register"
+  
 
   return (
     <div className='sign-up-screen'>
       <Header headerTitle={headerTitle} headerText={headerText}/>
-      <form onSubmit={onSignUp}>
-        <label htmlFor="first_name">What's your first name?</label>
-        <input 
-          type="text"
-          name="first_name"
-          value={first_name}
-          onChange={handleChange}
-        />
-        <label htmlFor="last_name">What's your last name?</label>
-        <input 
-          type="text"
-          name="last_name"
-          value={last_name}
-          onChange={handleChange} 
-        />
-        <label htmlFor="email">What's your email?</label>
-        <input 
-          type="email" 
-          name="email"
-          value={email}
-          onChange={handleChange}
-        />
-        <label htmlFor="password">Create a password:</label>
-        <input 
-          type="password" 
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-        <label htmlFor="passwordConfirm">Confirm your password:</label>
-        <input 
-          type="password" 
-          name="passwordConfirm"
-          value={passwordConfirm}
-          onChange={handleChange} 
-        />
-        <button type="submit">Register</button>
-      </form>
+      <Form onSubmit={handleSubmit} inputs={inputs} buttonText={buttonText} />
     </div>
   )
 }
+
