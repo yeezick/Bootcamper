bb70faa890412ddb7747be54870b939adf7801ac
Nicholas Rynearson <nrynearson@gmail.com>
2022-01-26 16:02:19 -0500

    replaces div in Header component with header tag

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import './Header.scss'
 export const Header = ({ headerTitle, headerText }) => {
 
   return ( 
-  <div className='header'>
+  <header className='header'>
     <h3 className='header-title'>{headerTitle}</h3>
     <p className="header-text">{headerText}</p>
-  </div>
+  </header>
   )
 };
