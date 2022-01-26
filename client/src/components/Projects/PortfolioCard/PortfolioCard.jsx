809177384f0592fa93c034fe72a706084d44f270
yeezick <ramerick5@gmail.com>
2022-01-26 18:03:37 -0500

    refactoring edit/create profile
    - create and style modal component
    - relocate  to the portfoliocard component
    - add subtle styling to the divs on this screen

--- client/src/components/Projects/PortfolioCard/PortfolioCard.jsx
+++ client/src/components/Projects/PortfolioCard/PortfolioCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './PortfolioCard.scss';
 
 const dummyProjects = [
@@ -20,13 +21,100 @@ const dummyProjects = [
     link: 'www.google.com',
   },
 ];
+// make this a global function and remove
+const handleChange = (e, property, setterFunction) => {
+  const { value } = e.target;
+  setterFunction((state) => {
+    return { ...state, [property]: value };
+  });
+};
+
+export const AddPortfolioProject = () => {
+  const [newProject, setNewProject] = useState({
+    description: '',
+    link: '',
+    title: '',
+  });
+  // ideally updates the database on each new project without slowing the app down
+  // this way the user can add a new project and on refresh, load their work.
+  const handleSubmit = (e) => {
+    console.log(e);
+  };
+
+  const formData = {
+    button: {
+      type: 'single',
+      text: 'Add New Project',
+    },
+    handlers: {
+      onChange: handleChange,
+      // onSubmit: handleNewProject,
+    },
+    inputs: [
+      {
+        labelText: 'Project Title',
+        name: 'project_title',
+        type: 'text',
+        value: newProject.title,
+      },
+      {
+        labelText: 'Describe the project',
+        name: 'project_description',
+        type: 'textarea',
+        value: newProject.description,
+      },
+      {
+        labelText: 'Link to your project',
+        name: 'project_link',
+        type: 'text',
+        value: newProject.link,
+      },
+    ],
+  };
+
+  return (
+    <div className="add-portfolio-project">
+      <header>
+        Add your personal projects here. You can add as many as you want, but to avoid cluttering we
+        recommend a maximum of 3.
+      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Project Title:
+          <input
+            type="text"
+            value={newProject.title}
+            onChange={(e) => handleChange(e, 'title', setNewProject)}
+          />
+        </label>
+
+        <label>
+          Describe the project. <small>(max 250 characters)</small>
+          <input
+            type="text"
+            value={newProject.description}
+            onChange={(e) => handleChange(e, 'description', setNewProject)}
+          />
+        </label>
+
+        <label>
+          Link to your project
+          <input
+            type="text"
+            value={newProject.link}
+            onChange={(e) => handleChange(e, 'link', setNewProject)}
+          />
+        </label>
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
 
 export const ShowPortfolioProjects = () => {
-  // map through all user projects
-  // return portfolio card for that project
   return (
-    <div>
-      <header> Your Projects</header>
+    <div className="show-portfolio-wrapper">
+      <header> Portfolio Projects</header>
       {dummyProjects.map((project, idx) => (
         <PortfolioProject key={idx} project={project} />
       ))}
@@ -41,6 +129,7 @@ const PortfolioProject = ({ project }) => {
       <img src={image} alt={title} />
       <div className="portfolio-content">
         <p>{title}</p>
+        {/* there should be an expand description that would expand the card and show the the project's description */}
         <p>{description}</p>
         <p>{link}</p>
       </div>
--- client/src/screens/EditProfile/EditProfile.jsx
+++ client/src/screens/EditProfile/EditProfile.jsx
@@ -1,26 +1,36 @@
 import { useState } from 'react';
-import { ShowPortfolioProjects } from '../../components/Projects/PortfolioCard/PortfolioCard';
+import {
+  AddPortfolioProject,
+  ShowPortfolioProjects,
+} from '../../components/Projects/PortfolioCard/PortfolioCard';
+import { Modal } from '../../components/Modal/Modal';
+import './EditProfile.scss';
 
+// make this a global function and remove
 const handleChange = (e, property, setterFunction) => {
   const { value } = e.target;
-
   setterFunction((state) => {
     return { ...state, [property]: value };
   });
 };
 
 export const EditProfile = () => {
+  const [showModal, setShowModal] = useState(true);
   return (
-    <div>
-      <header>
-        (header component) About You - Before you can create or join a project, we'll need to finish
-        your profile first.
-      </header>
-      <AboutUser />
-      <AddPortfolioProject />
-      <ShowPortfolioProjects />
-      <button>Update Profile</button>
-    </div>
+    <>
+      {showModal && <Modal setShowModal={setShowModal} />}
+      <button onClick={() => setShowModal(!showModal)}>show modal</button>
+      <div className="edit-profile">
+        <header>
+          (header component) About You - Before you can create or join a project, we'll need to
+          finish your profile first.
+        </header>
+        <AboutUser />
+        <AddPortfolioProject />
+        <ShowPortfolioProjects />
+        <button>Update Profile</button>
+      </div>
+    </>
   );
 };
 
@@ -28,110 +38,51 @@ const AboutUser = () => {
   const [userInfo, setUserInfo] = useState({
     about: '',
     funFact: '',
-    role: '',
     portfolio_link: '',
+    role: '',
     user_projects: [],
   });
-  const [dropdownOption, setDropdownOption] = useState('');
-
-  const handleDropdownChange = (e) => {
-    const { value } = e.target;
-    setDropdownOption(value);
-  };
 
   return (
-    <div>
-      <div>
-        <label>
-          Tell us a bit about you <small>(max 250 characters)</small>
-          <input
-            type="textarea"
-            max={250}
-            value={userInfo.about}
-            onChange={(e) => handleChange(e, 'about', setUserInfo)}
-          />
-        </label>
+    <div className="about-user">
+      <label>
+        Tell us a bit about you <small>(max 250 characters)</small>
+        <input
+          type="textarea"
+          max={250}
+          value={userInfo.about}
+          onChange={(e) => handleChange(e, 'about', setUserInfo)}
+        />
+      </label>
 
-        <label>
-          Include a fun fact! <small> (optional, max 250 characters)</small>
-          <input
-            type="texarea"
-            max={250}
-            value={userInfo.funFact}
-            onChange={(e) => handleChange(e, 'funFact')}
-          />
-        </label>
+      <label>
+        Include a fun fact! <small> (optional, max 250 characters)</small>
+        <input
+          type="texarea"
+          max={250}
+          value={userInfo.funFact}
+          onChange={(e) => handleChange(e, 'funFact', setUserInfo)}
+        />
+      </label>
 
-        <label>
-          I am a...
-          <select value={dropdownOption} onChange={handleDropdownChange}>
-            <option value="UX designer">UX designer</option>
-            <option value="Software engineer">Software Engineer</option>
-          </select>
-        </label>
+      <label>
+        I am a...
+        <select defaultValue="" onChange={(e) => handleChange(e, 'role', setUserInfo)}>
+          <option value="UX designer">UX designer</option>
+          <option value="Software engineer">Software Engineer</option>
+        </select>
+      </label>
 
-        <label>
-          Link to your portfolio <small>(optional)</small>
-          <input
-            type="text"
-            value={userInfo.portfolio_link}
-            onChange={(e) => handleChange(e, 'portfolio_link')}
-          />
-        </label>
-        {/* save button should save the user's progress so they don't lose this on refresh */}
-        <button>save</button>
-      </div>
-    </div>
-  );
-};
-
-const AddPortfolioProject = () => {
-  const [newProject, setNewProject] = useState({
-    title: '',
-    description: '',
-    link: '',
-  });
-  // ideally updates the database on each new project without slowing the app down
-  // this way the user can add a new project and on refresh, load their work.
-  const handleChange = (e, property) => {
-    const { value } = e.target;
-    setNewProject((state) => {
-      return { ...state, [property]: value };
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(e);
-  };
-
-  return (
-    <div>
-      <header>
-        Add your personal projects here. You can add as many as you want, but to avoid cluttering we
-        recommend a maximum of 3.
-      </header>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Project Title:{' '}
-          <input type="text" value={newProject.title} onChange={(e) => handleChange(e, 'title')} />
-        </label>
-
-        <label>
-          Describe the project. <small>(max 250 characters)</small>
-          <input
-            type="text"
-            value={newProject.description}
-            onChange={(e) => handleChange(e, 'description')}
-          />
-        </label>
-
-        <label>
-          Link to your project
-          <input type="text" value={newProject.link} onChange={(e) => handleChange(e, 'link')} />
-        </label>
-        <button>Add</button>
-      </form>
+      <label>
+        Link to your portfolio <small>(optional)</small>
+        <input
+          type="text"
+          value={userInfo.portfolio_link}
+          onChange={(e) => handleChange(e, 'portfolio_link', setUserInfo)}
+        />
+      </label>
+      {/* save button should save the user's progress by updating it in the database so they don't lose this on refresh */}
+      <button>save</button>
     </div>
   );
 };
