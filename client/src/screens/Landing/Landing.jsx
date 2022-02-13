a597966e563ef8c583cc838fcbcf12fa3c322e84
gooliajulia929 <juliadwyer93@gmail.com>
2022-02-13 13:06:52 -0500

    create Landing scss file and add Landing screen basic structure

--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export const Landing = () => {
   return (
-    <div>
-      Landing Page
+    <div className="landing-page">
+      <div className="img-placeholder">
+      </div>
+      <h1 className="logo-placeholder">Bootcamper</h1>
+      <button>Sign Up</button>
+      <button>Log In</button>
+      <h6>Help | Contact</h6>
     </div>
   )
 }
