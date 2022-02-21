feea1679bc9311a9c6e120c2dee7a9bcde9aa614
gooliajulia929 <juliadwyer93@gmail.com>
2022-02-20 19:13:25 -0500

    Add confirm password error handling, remove header and change title to match Figma

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Header } from '../../components/Header/Header.jsx';
 import { Form } from '../../components/Form/Form.jsx';
 // assets
 import { signUpForm } from '../../services/formData.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, getState } from 'react-redux';
 import { signUpUser } from '../../services/redux/slices/uiActions.js';
+import { GenericModal } from '../../components/Modal/GenericModal.jsx';
+import './SignUp.scss';
+
 
 export const SignUp = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState({
+    presence: false,
+    msg: '',
+  });
   const [newUser, setNewUser] = useState({
-    confirmPassword: '',
+    confirm_password: '',
     email: '',
     first_name: '',
     last_name: '',
@@ -20,18 +27,45 @@ export const SignUp = () => {
 
   const header = {
     text: "In order to find projects and connect with other users, we'll need to create an account.",
-    title: 'Register',
+    title: 'Create an account',
   };
+
 
   const handleSignUp = async (event) => {
     event.preventDefault();
-    dispatch(signUpUser(newUser));
-    navigate('/');
+    if (newUser.confirm_password !== newUser.password) {
+      setNewUser(prevState => {
+        return {
+          ...prevState,
+          confirm_password: '',
+          password: '',
+        }
+      });
+      setError({
+        presence: true,
+        msg: 'Passwords do not match'})
+      setShowModal(true)
+    } else {
+      dispatch(signUpUser(newUser));
+    }
+
+      // navigate('/');
+      // setError({
+      //   presence: true,
+      //   msg: this.err,
+      // });
+      // setShowModal(true);
+    
   };
 
   return (
     <div className="sign-up-screen">
-      <Header headerText={header.text} headerTitle={header.title} />
+      {showModal && 
+        <GenericModal 
+          bodyText={error.msg} 
+          buttonText="Try again" 
+          setShowModal={setShowModal}/>  }
+          <h4>Create an account</h4>
       <Form formData={signUpForm} formState={[newUser, setNewUser, handleSignUp]} />
     </div>
   );
