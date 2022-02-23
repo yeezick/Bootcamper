2807e5c14b16b5133dbfdba6705253a183f1bbed
gooliajulia929 <juliadwyer93@gmail.com>
2022-02-23 18:30:50 -0500

    handle no account and invalid credentials errors

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,35 +1,105 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Header } from '../../components/Header/Header.jsx';
-import { Form } from '../../components/Form/Form';
 // assets
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../services/redux/slices/uiActions.js';
-import { signInForm } from '../../services/formData.js';
+import '../SignUp/SignUp.scss';
+import store from '../../services/redux/store.js'
+import { GenericModal } from '../../components/Modal/GenericModal';
+import { handleChange } from '../../services/utils/formHandlers';
+import { SingleActionButton } from '../../components/Button/SingleActionButton';
+import { checkEmailAuth, signOut } from '../../services/api/users';
 
 export const SignIn = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [noAccountError, setNoAccountError] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [loginInfo, setLoginInfo] = useState({
     email: 'test@test.com',
     password: 'test',
   });
 
+  useEffect( async () => {
+    signOut();
+  },[])
+
   const handleSignIn = async (event) => {
     event.preventDefault();
-    dispatch(loginUser(loginInfo));
-    navigate('/');
+    if (!noAccountError) {
+      await dispatch(loginUser(loginInfo));
+      //check for user
+      const state = store.getState();
+      if (state.ui.user.email === loginInfo.email) {
+        navigate(`/users/${state.ui._id}/edit`);
+      } else {
+        setShowModal(true);
+        setLoginInfo(prevState => {
+          return {
+            ...prevState,
+            password: '',
+          }
+        })
+      }
+    } else {
+      setShowModal(true);
+      setLoginInfo((prevState) => {
+        return {
+          ...prevState,
+          password: '',
+        }
+      })
+    }
+    
   };
 
-  const header = {
-    text: "Welcome back! Let's get you signed in!",
-    title: 'Sign In',
-  };
+  const validEmail = async () => {
+    const emailReq = { email: loginInfo.email }
+    const res = await checkEmailAuth(emailReq);
+    if (!res) {
+      setNoAccountError(true);
+    }
+  }
+
 
   return (
-    <div className="sign-in-screen">
-      <Header headerText={header.text} headerTitle={header.title} />
-      <Form formData={signInForm} formState={[loginInfo, setLoginInfo, handleSignIn]} />
+    <div className="sign-in-screen auth-form">
+      {showModal && <GenericModal
+        setShowModal={setShowModal}
+        bodyText="Invalid credentials. Please check your info try again, or register a new account."
+        buttonText="Ok"
+      />}
+      <h4>Welcome Back!</h4>
+      <form className="form sign-in" onSubmit={handleSignIn}>
+        <div className="input-wrapper">
+              <label htmlFor="email">Email</label>
+              <input 
+                id="email"
+                name="email"
+                onChange={(e) => handleChange(e, "email", setLoginInfo)}
+                type="text"
+                value={loginInfo["email"]}
+                onFocus={()=> setNoAccountError(false)}
+                onBlur={() => validEmail()}
+                />
+            </div>
+            <div className="form-error">
+              { noAccountError && <h6>User not found.</h6> }
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input 
+                id="password"
+                name="password"
+                onChange={(e) => handleChange(e, "password", setLoginInfo)}
+                type="password"
+                value={loginInfo["password"]}
+                />
+          </div>
+          <SingleActionButton text="Log In" type="submit" />
+      </form>
+      {/* Placeholder for future functionality  */}
+      <a href="#">Forgot Password?</a>
     </div>
   );
 };
