abe02c424f91254f156114f0eec86720046558a3
gooliajulia929 <juliadwyer93@gmail.com>
2022-02-23 12:54:28 -0500

    create double action button modal component

--- client/src/components/Button/DoubleActionButton.jsx
+++ client/src/components/Button/DoubleActionButton.jsx
@@ -1,9 +1,9 @@
 import './DoubleActionButton.scss';
 
 
-export const DoubleActionButton = ({leftText, leftOnClick, leftType="button", rightText, rightOnClick, rightType="button" }) => {
+export const DoubleActionButton = ({leftText, leftOnClick, leftType="button", rightText, rightOnClick, rightType="button", onClick }) => {
     return (
-        <div className="double-button">
+        <div className="double-button" onClick={onClick}>
             <button className="left" onClick={leftOnClick} type={leftType} >{leftText}</button>
             <div className="separator">|</div>
             <button className="right" onClick={rightOnClick} type={rightType} value={rightText}>{rightText}</button>
---
+++ client/src/components/Modal/DoubleActionModal.jsx
@@ -0,0 +1,24 @@
+import { DoubleActionButton } from '../Button/DoubleActionButton';
+import './Modal.scss';
+
+export const DoubleActionModal = ({ setShowModal, bodyText, leftText, leftOnClick, rightText, rightOnClick }) => {
+    const handleModal = () => {
+        setShowModal((state) => !state);
+    };
+    return (
+        <div className="modal-background">
+        <div className="modal-wrapper">
+            <div className="modal-content">
+            <p>{bodyText}</p>
+            <DoubleActionButton 
+                leftText={leftText} 
+                leftOnClick={leftOnClick}
+                rightText={rightText}
+                rightOnClick={rightOnClick}
+                onClick={handleModal} 
+                />
+            </div>
+        </div>
+        </div>
+    );
+    };
