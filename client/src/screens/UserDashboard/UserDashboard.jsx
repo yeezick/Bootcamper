188348ae5dfed34f8a5064032f083eafbceb96e8
Nicholas Rynearson <nrynearson@gmail.com>
2022-02-23 17:07:44 -0500

    small styling changes

--- client/src/screens/UserDashboard/UserDashboard.jsx
+++ client/src/screens/UserDashboard/UserDashboard.jsx
@@ -23,17 +23,17 @@ export const UserDashboard = () => {
       <div className="collaborations-wrapper">
         <h3>My Collaborations:</h3>
         {collaborations?.map(project => (
-          <div className="collaboration-wrapper" key={project._id}>
-            <Link to={`/projects/${project._id}`}><h4>{project.title}</h4></Link>
-          </div>
+          <Link to={`/projects/${project._id}`}><div className="collaboration-wrapper" key={project._id}>
+            <h4>{project.title}</h4>
+          </div></Link>
         ))}
       </div>
       <div className="user-projects-wrapper">
         <h3>My Projects:</h3>
           {userProjects.map(project => (
-            <div className="user-project-wrapper" key={project._id}>
-              <Link to={`/projects/${project._id}`}><h4>{project.title}</h4></Link>
-            </div>
+            <Link to={`/projects/${project._id}`}><div className="user-project-wrapper" key={project._id}>
+              <h4>{project.title}</h4>
+            </div></Link>
           ))}
       </div>
     </div>
