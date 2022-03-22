972215461fd1daf24d4096f9fcac2e92ab3570af
yeezick <ramerick5@gmail.com>
2022-03-21 20:00:17 -0400

    begin the transition for PortfolioCard - this file should be broken up or renamed, 3 components live here and the name is mis leading - missing form component conversion - needs connection to passed props such as currProject

--- native/components/Projects/PortfolioCard/PortfolioCard.jsx
+++ native/components/Projects/PortfolioCard/PortfolioCard.jsx
@@ -10,8 +10,9 @@ import { addPortfolioProject } from '../../../services/api/users.js';
 import './PortfolioCard.scss';
 import { handleChange } from '../../../services/utils/formHandlers';
 import { addRejectedProject } from '../../../services/redux/actions/uiActions';
-
-export const AddPortfolioProject = () => {
+import { Button, View } from 'react-native';
+/*
+export const REACTAddPortfolioProject = () => {
   const { _id: userId } = useSelector((state) => state.ui.user);
   const dispatch = useDispatch();
   const [newProject, setNewProject] = useState({
@@ -55,7 +56,27 @@ export const AddPortfolioProject = () => {
     </div>
   );
 };
+*/
 
+export const ShowPortfolioProjects = () => {
+  return (
+    currUser?.portfolio_projects?.length > 0 && (
+      // className="show-portfolio-wrapper
+      <View>
+        <Text> Your Portfolio Projects</Text>
+        {currUser?.portfolio_projects?.map((project, idx) => (
+          <PortfolioProject
+            key={`portfolioProject-${idx}`}
+            project={project}
+            updateEditedProject={updateEditedProject}
+          />
+        ))}
+      </View>
+    )
+  );
+};
+
+/*
 export const ShowPortfolioProjects = ({ currUser }) => {
   const dispatch = useDispatch();
 
@@ -89,7 +110,92 @@ export const ShowPortfolioProjects = ({ currUser }) => {
     )
   );
 };
+*/
 
+export const PortfolioProject = () => {
+  const [currProject, setCurrProject] = useState({
+    image: 'https://pbs.twimg.com/media/E5KGFT9X0AQzzaR?format=jpg&name=240x240',
+    project_description: '',
+    project_link: '',
+    project_title: '',
+    project_id: nanoid(),
+  });
+  const [editProject, toggleEditProject] = useState(false);
+  const { image, project_description, project_link, project_title } = currProject;
+
+  useEffect(() => {
+    const onLoad = () => {
+      setCurrProject(project);
+    };
+    onLoad();
+  }, [project]);
+
+  const handleProjectUpdate = (e, removeProject) => {
+    updateEditedProject(currProject, removeProject);
+    toggleEditProject(!editProject);
+  };
+
+  if (editProject) {
+    return (
+      // className="edit-portfolio-project"
+      <View>
+        <Button onPress={() => toggleEditProject(!editProject)}>toggle edit</Button>
+        <Button onPress={(e) => handleProjectUpdate(e, 'remove project')}>delete</Button>
+        {/* reuse of inputs is opportunity to consolidate using a map method  */}
+        <label className="">
+          Title:
+          <input
+            type="text"
+            className=""
+            value={project_title}
+            onChange={(e) => handleChange(e, 'project_title', setCurrProject)}
+          />
+        </label>
+
+        <label className="">
+          Description:
+          <input
+            type="text"
+            className=""
+            value={project_description}
+            onChange={(e) => handleChange(e, 'project_description', setCurrProject)}
+          />
+        </label>
+
+        <label className="">
+          Link:
+          <input
+            type="text"
+            className=""
+            value={project_link}
+            onChange={(e) => handleChange(e, 'project_link', setCurrProject)}
+          />
+        </label>
+        <Button onPress={handleProjectUpdate}> SAVE EDIT</Button>
+      </View>
+    );
+  }
+
+  return (
+    // className="portfolio-project"
+    <View>
+      <Button
+        style={{ width: '45px' }}
+        onPress={() => {
+          toggleEditProject(!editProject);
+        }}
+      ></Button>
+      <Text>I AM AN IMAGE</Text>
+      {/* className="portfolio-content" */}
+      <View>
+        <Text>{project_title}</Text>
+        <Text>{project_description}</Text>
+        <Text>{project_link}</Text>
+      </View>
+    </View>
+  );
+};
+/* 
 const PortfolioProject = ({ updateEditedProject, project }) => {
   const [currProject, setCurrProject] = useState({
     image: 'https://pbs.twimg.com/media/E5KGFT9X0AQzzaR?format=jpg&name=240x240',
@@ -118,7 +224,7 @@ const PortfolioProject = ({ updateEditedProject, project }) => {
       <div className="edit-portfolio-project">
         <button onClick={() => toggleEditProject(!editProject)}>toggle edit</button>
         <button onClick={(e) => handleProjectUpdate(e, 'remove project')}>delete</button>
-        {/* reuse of inputs is opportunity to consolidate using a map method */}
+      ///  {/* reuse of inputs is opportunity to consolidate using a map method 
         <label className="">
           Title:
           <input
@@ -163,10 +269,10 @@ const PortfolioProject = ({ updateEditedProject, project }) => {
       <img src={image} alt={project_title} />
       <div className="portfolio-content">
         <p>{project_title}</p>
-        {/* there should be an expand description that would expand the card and show the the project's description */}
         <p>{project_description}</p>
         <p>{project_link}</p>
       </div>
     </div>
   );
 };
+*/
