6e659280af40068c84713b5504cbf8759182386c
yeezick <ramerick5@gmail.com>
2022-03-26 15:44:51 -0400

    restoring handleTextChange to avoid rewriting state

--- native/services/utils/formHandlers.js
+++ native/services/utils/formHandlers.js
@@ -1,5 +1,5 @@
 export const handleTextChange = (text, property, setterFunction) => {
   setterFunction((state) => {
-    return { state, [property]: text };
+    return { ...state, [property]: text };
   });
 };
