5dd376c141a23a004bb245228e6f3d4dfec0e332
gooliajulia929 <juliadwyer93@gmail.com>
2022-03-28 18:53:45 +0200

    remove commented out non native code

--- native/screens/SignIn.jsx
+++ native/screens/SignIn.jsx
@@ -1,15 +1,9 @@
-
-// import { useNavigate } from "react-router-dom";
-// // assets
-// import { useDispatch, useSelector } from "react-redux";
-// import { loginUser } from "../../services/redux/slices/uiActions.js";
-// import "../SignUp/SignUp.scss";
-// import { handleChange } from "../../services/utils/formHandlers";
-// import { SingleActionButton } from "../components/Button/SingleActionButton";
-// import { checkEmailAuth, signOut, verify } from "../../services/api/users";
+import { useState, createRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { signOut, checkEmailAuth, verify } from '../services/api/users.js';
 import { loginUser } from '../services/redux/actions/uiActions.js';
+
+// Native Components
 import { 
   StyleSheet, 
   Text, 
@@ -19,19 +13,23 @@ import {
   Button,
   Alert,
 } from "react-native";
-import { useState, createRef, useEffect } from 'react';
+
 
 export const SignIn = ({navigation}) => {
+  // redux
   const dispatch = useDispatch();
+  // error states
   const [noAccountError, setNoAccountError] = useState(null);
   const [authError, setAuthError] = useState(null);
+  // log in state
   const [logInInfo, setLogInInfo] = useState({
     email: '',
     password: '',
   });
-
+  // input references
   const { emailInputRef, passwordInputRef } = createRef();
 
+  // sign out user on sign in screen render
   useEffect(() => {
     signOut();
   }, []);
@@ -44,6 +42,7 @@ export const SignIn = ({navigation}) => {
         email: '',
         password: '',
       });
+      // check this params functionality on edit land
       navigation.navigate('EditProfile', {
         id: user._id,
       })
@@ -165,88 +164,3 @@ const styles = StyleSheet.create({
     fontWeight: '500',
   }
 })
-
-// export const ReactSignIn = () => {
-//   const navigate = useNavigate();
-//   const dispatch = useDispatch();
-//   const { _id: userId } = useSelector((state) => state.ui.user);
-//   const [authError, setAuthError] = useState(null);
-//   const [noAccountError, setNoAccountError] = useState(null);
-//   const [loginInfo, setLoginInfo] = useState({
-//     email: "test@test.com",
-//     password: "test",
-//   });
-
-  // useEffect(() => {
-  //   signOut();
-  // }, []);
-
-  // const handleSignIn = async (event) => {
-  //   event.preventDefault();
-  //   await dispatch(loginUser(loginInfo));
-  //   const user = await verify();
-  //   if (user.email === loginInfo.email) {
-  //     navigate(`/users/${userId}/edit`);
-  //   } else {
-  //     setAuthError(
-  //       "Invalid credentials. Please check your details and try again."
-  //     );
-  //     setLoginInfo((prevState) => {
-  //       return {
-  //         ...prevState,
-  //         password: "",
-  //       };
-  //     });
-  //   }
-  // };
-
-  // const validEmail = async () => {
-  //   const emailReq = { email: loginInfo.email };
-  //   const res = await checkEmailAuth(emailReq);
-  //   if (!res) {
-  //     setNoAccountError("Account not found.");
-  //   }
-  // };
-
-//   return (
-//     <div className="sign-in-screen auth-form">
-//       <h4>Welcome Back!</h4>
-//       <form className="form sign-in" onSubmit={handleSignIn}>
-//         <div className="input-wrapper">
-//           <label htmlFor="email">Email</label>
-//           <input
-//             required
-//             id="email"
-//             name="email"
-//             onChange={(e) => handleChange(e, "email", setLoginInfo)}
-//             type="email"
-//             value={loginInfo["email"]}
-//             onFocus={() => setNoAccountError(false)}
-//             onBlur={() => validEmail()}
-//           />
-//         </div>
-//         <div className="form-error">
-//           <h6>{noAccountError}</h6>
-//         </div>
-//         <div className="input-wrapper">
-//           <label htmlFor="password">Password</label>
-//           <input
-//             required
-//             id="password"
-//             name="password"
-//             onChange={(e) => handleChange(e, "password", setLoginInfo)}
-//             type="password"
-//             value={loginInfo["password"]}
-//             onFocus={() => setAuthError(null)}
-//           />
-//         </div>
-//         <div className="form-error">
-//           <h6>{authError}</h6>
-//         </div>
-//         <SingleActionButton text="Log In" type="submit" />
-//       </form>
-//       {/* Placeholder for future functionality  */}
-//       <a href="#">Forgot Password?</a>
-//     </div>
-//   );
-// };
