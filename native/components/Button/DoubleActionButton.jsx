1a7002620e09d970cd1fdc0190b27578e626c5ff
yeezick <ramerick5@gmail.com>
2022-04-03 00:51:53 -0400

    roulette and double action button are renderable in native, lacking funcitonality testing

--- native/components/Button/DoubleActionButton.jsx
+++ native/components/Button/DoubleActionButton.jsx
@@ -1,34 +1,26 @@
-import { Button } from "react-native";
-import { Alert } from "react-native-web";
-import "./DoubleActionButton.scss";
+import { Button, Text, View } from 'react-native';
+import { Alert } from 'react-native-web';
 
 export const DoubleActionButton = ({
   leftText,
   leftOnClick,
-  leftType = "button",
+  leftType = 'button',
   rightText,
   rightOnClick,
-  rightType = "button",
+  rightType = 'button',
   onClick,
 }) => {
   return (
-    <div className="double-button" onClick={onClick}>
+    <View className="double-button" onClick={onClick}>
+      <Button className="left" onClick={leftOnClick} title={leftText} type={leftType} />
+      <Text className="separator">|</Text>
       <Button
-        title="button"
-        onPress={() => Alert.alert("Simple button pressed")}
-      />
-      <button className="left" onClick={leftOnClick} type={leftType}>
-        {leftText}
-      </button>
-      <div className="separator">|</div>
-      <button
         className="right"
         onClick={rightOnClick}
+        title={rightText}
         type={rightType}
         value={rightText}
-      >
-        {rightText}
-      </button>
-    </div>
+      />
+    </View>
   );
 };
