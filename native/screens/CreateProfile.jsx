cd70c88a6d2e479d582efda03e459d75532d4cb4
gooliajulia929 <juliadwyer93@gmail.com>
2022-05-02 21:00:24 -0400

    sets up CreateProfile screen as part one of new user profile flow

--- native/screens/CreateProfile.jsx
+++ native/screens/CreateProfile.jsx
@@ -1,51 +1,15 @@
 // import React from 'react'
-import { useState, useEffect } from 'react';
-import { Text, ScrollView, View } from 'react-native';
+import { useState} from 'react';
+import { ScrollView } from 'react-native';
 import { Header } from '../components/Header/Header';
 import { useSelector, useDispatch } from 'react-redux';
 import { uiActions } from '../services/redux/slices/uiSlice';
 import { updateUser } from '../services/api/users';
-import { userForm } from '../services/formData';
 import { Form } from '../components/Form/Form';
-import { AddPortfolioProject } from '../components/PortfolioCard/AddPortfolioProject';
-import { ShowPortfolioProjects } from '../components/PortfolioCard/ShowPortfolioProjects';
-import { SingleActionButton } from '../components/Button/SingleActionButton';
+import { createProfile } from '../services/formData';
 
-export const CreateProfile = () => {
-    const { user: reduxUser, editMode } = useSelector((state) => state.ui);
-    const dispatch = useDispatch();
-    const [ isCreated, setIsCreated ] = useState(false);
-
-
-    const finish = {
-        text: 'Finish',
-        type: 'trigger-alert',
-        options: [
-            {
-                
-            }
-        ],
-    }
-
-    return (
-        <ScrollView>
-            { !isCreated ? 
-                <AboutUser setIsCreated={setIsCreated} /> 
-                :
-                (
-                <>
-                    <AddPortfolioProject />
-                    <SingleActionButton payload={finish} />
-                </>)
-            }
-            
-        </ScrollView>
-    )
-};
-
-
-const AboutUser = ({setIsCreated}) => {
-    const { editMode, user } = useSelector((state) => state.ui);
+export const CreateProfile = ({navigation}) => {
+    const { user } = useSelector((state) => state.ui);
     const dispatch = useDispatch();
     const [userInfo, setUserInfo] = useState({
         role: '',
@@ -54,39 +18,20 @@ const AboutUser = ({setIsCreated}) => {
         show_portfolio: false,
         });
     
-        useEffect(() => {
-        if (editMode) {
-            const { about, fun_fact, portfolio_link, role } = user;
-            setUserInfo({
-            about,
-            fun_fact,
-            portfolio_link,
-            role,
-            });
-        }
-        }, [editMode]);
-    
-        const handleUserUpdate = async () => {
+    const handleUserUpdate = async () => {
         try {
             const res = await updateUser(user._id, userInfo);
             dispatch(uiActions.updateUser(res));
-            setIsCreated(true);
+            navigation.navigate('AddPortfolioProjects')
         } catch (error) {
             console.error(error);
         }
-        };
-
-        const header = {
-            title: "Create Profile",
-            text: null,
-        }
-    
-        return (
-        // className="about-user"
-        <View>
-            <Header headerTitle={header.title} headerText={header.text} />
-            <Form formData={userForm} formState={[userInfo, setUserInfo, handleUserUpdate]} />
-        </View>
-        );
     };
-    
+
+    return (
+        <ScrollView>
+            <Header headerTitle="Create Profile" headerText="" />
+            <Form formData={createProfile} formState={[userInfo, setUserInfo, handleUserUpdate]} />
+        </ScrollView>
+    )
+};
