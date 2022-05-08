ad88f9b281eead0023fe6f98d9d517c073a7c666
yeezick <ramerick5@gmail.com>
2022-05-08 15:20:11 -0400

    - create UpdatePasswordForm component and render it conditionally   - feels WET and is not functional yet

--- native/screens/AccountSettings.jsx
+++ native/screens/AccountSettings.jsx
@@ -1,11 +1,62 @@
+import { useState } from 'react';
 import { Button, Text, View } from 'react-native';
+import { handleTextChange, handleToggle } from '../services/utils/handlers';
 
-export const AccountSettings = () => {
+export const AccountSettings = ({ route }) => {
+  const [resetPassword, toggleResetPassword] = useState(false);
+
   return (
     <View>
       <Text>Account Settings</Text>
-      <Text>Reset Password</Text>
+      <Button
+        title={resetPassword ? 'Cancel' : 'Reset password'}
+        onPress={() => handleToggle(toggleResetPassword)}
+      />
+
+      {resetPassword && (
+        <UpdatePasswordForm
+          toggleResetPassword={toggleResetPassword}
+          userID={route.params.userID}
+        />
+      )}
+
       <Button title="Delete Profile" />
+    </View>
+  );
+};
+
+const UpdatePasswordForm = ({ toggleResetPassword, userID }) => {
+  const [newPasswordForm, setNewPasswordForm] = useState({
+    confirmNewPassword: '',
+    currentPassword: '',
+    newPassword: '',
+  });
+
+  // this feels WET
+  return (
+    <View>
+      <Text>Confirm your password:</Text>
+      <TextInput
+        onChangeText={(currPassword) =>
+          handleTextChange(currPassword, 'currentPassword', setNewPasswordForm)
+        }
+        value={newPasswordForm.currentPassword}
+      />
+      <Text>Create new password:</Text>
+      <TextInput
+        onChangeText={(newPassword) =>
+          handleTextChange(newPassword, 'newPassword', setNewPasswordForm)
+        }
+        value={newPasswordForm.newPassword}
+      />
+      <Text>Confirm new password:</Text>
+      <TextInput
+        onChangeText={(confirmNewPassword) =>
+          handleTextChange(confirmNewPassword, 'confirmNewPassword', setNewPasswordForm)
+        }
+        value={newPasswordForm.confirmNewPassword}
+      />
+      <Button title="Update my password" onPress={() => handleToggle(toggleResetPassword)} />
     </View>
   );
 };
