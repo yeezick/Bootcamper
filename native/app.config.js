69d93b48a540bf50e7022b092c62d60f25a0e275
yeezick <ramerick5@gmail.com>
2022-06-26 14:33:03 -0400

    - update app.json to app.config.js to allow env variables to be accessible throughout the app and enable configuring app settings like icons etc - enable firebase app in config folder - install chat messaging dependencies - update .gitignore to include .env file -> need to integrate 1password service for this

---
+++ native/app.config.js
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'native',
+    slug: 'native',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/jira-rocket.png', //todo: image will most likely be copyrighted
+    splash: {
+      image: './assets/jira-rocket.png', // the image that pops up on initial app load
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff'
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/jira-rocket.png',
+        backgroundColor: '#FFFFFF'
+      }
+    },
+    web: {
+      favicon: './assets/jira-rocket .png'
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
+  }
+};
