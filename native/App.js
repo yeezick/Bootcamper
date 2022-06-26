74576b46b9dcfce4a12af68b9e507d54964e6d2e
yeezick <ramerick5@gmail.com>
2022-06-26 16:53:53 -0400

    - create fireBase auth screens for login and signup - comment out app code on App.js to accommodate for this firebase tutorial for now

--- native/App.js
+++ native/App.js
@@ -1,14 +1,44 @@
-import 'react-native-gesture-handler';
-import { Provider } from 'react-redux';
-import store from './services/redux/store.js';
-import { SideMenu } from './components/SideMenu';
-import { Navbar } from './components/Navbar';
+// import 'react-native-gesture-handler';
+// import { Provider } from 'react-redux';
+// import store from './services/redux/store.js';
+// import { SideMenu } from './components/SideMenu';
+// import { Navbar } from './components/Navbar';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <Provider store={store}>
+//       <SideMenu />
+//       {/* <Navbar /> */}
+//     </Provider>
+//   );
+// }
+
+import { NavigationContainer, StackRouter } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SingleChat } from './screens/Messaging/SingleChat';
+
+const Stack = createStackNavigator();
+
+const ChatStack = () => {
+  // navigation limited to SingleChat
   return (
-    <Provider store={store}>
-      <SideMenu />
-      {/* <Navbar /> */}
-    </Provider>
+    <Stack.Navigator>
+      <Stack.Screen name="SingleChat" component={SingleChat} />
+    </Stack.Navigator>
   );
-}
+};
+
+const RootNavigator = () => {
+  // Leverage the ChatStack
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
+  return <RootNavigator />;
+};
+
+export default App;
