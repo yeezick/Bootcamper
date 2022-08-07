18ef59fea4ee7ca25502ab361c9bad4bbd73a270
Erick Manrique <ramerick5@gmail.com>
2022-08-07 17:48:16 -0400

    add test plan for signin screen'

---
+++ client/src/screens/SignIn/SignIn.test.js
@@ -0,0 +1,7 @@
+/**
+ * Renders correctly (snapshot test)
+ * Clicking signin button will:
+ *    - call the api
+ *    - save the user token into local storage
+ *    - navigate to user profile
+ */
