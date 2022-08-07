7ebefbeda8b69818f934f6eea6de9321d93e80a7
Erick Manrique <ramerick5@gmail.com>
2022-08-07 18:13:24 -0400

    add test plans for AddPortfolioProject, CreateProfile, UserProfile, and EditProfile

---
+++ client/src/screens/CreateProfile/CreateProfile.test.js
@@ -0,0 +1,5 @@
+/**
+ * Renders successfully
+ * OnClick "skip" will take user to "add projects to profile"
+ * OnClick "next" will save the info the user's data and navigate to the "add projects to profile" page
+ */
