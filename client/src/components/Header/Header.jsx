b2499d1ee0705866d4c6aba0e27e1d63dd73386a
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-30 14:26:31 -0400

    Changed heading tag to paragraph tag

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.scss';
 export const Header = ({ headerTitle }) => {
   return (
     <header className="header">
-      <h2 className="header-title">{headerTitle}</h2>
+      <p className="header-title">{headerTitle}</p>
     </header>
   );
 };
