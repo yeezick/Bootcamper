9f3a67e8854e63dc903b25f4bb87e2972fda91cb
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-30 15:08:58 -0400

    added js documentation

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import './Header.scss';
+
+/**
+ *
+ * @name Header
+ * @param {string} title - contains the title text of the Header
+ */
+
 export const Header = ({ headerTitle }) => {
   return (
     <header className="header">
