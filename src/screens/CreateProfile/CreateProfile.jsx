d99e5f7593bb43fbdd01dc5007c454d5967aaae0
Iffat Hossain <iffathossain77@gmail.com>
2022-09-12 18:26:00 -0400

    creates base for form

--- src/screens/CreateProfile/CreateProfile.jsx
+++ src/screens/CreateProfile/CreateProfile.jsx
@@ -11,135 +11,65 @@ import { updateUser } from '../../services/api/users';
 import './CreateProfile.scss';
 import { useEffect } from 'react';
 
+// assets
+
+import { loginUser } from '../../services/redux/actions/uiActions.js';
+import { checkEmailAuth, signOut, verify } from '../../services/api/users';
+
 export const CreateProfile = ({ currUser }) => {
+  // const [aboutMe, setAboutMe] = useState(null);
+  // const [link, setLink] = useState(null);
+
+
   const header = {
     text: "Before you can create or join a project, we'll need to finish your profile first.",
     title: 'About You',
   };
 
   return (
-    <>
-      <div className="create-profile">
-        <h3 className="header">Create Profile</h3>
-        <AboutUser />
-        <ShowPortfolioProjects currUser={currUser} />
-        <a className="later-link" href="/roulette">
-          Complete later
-        </a>
-      </div>
-    </>
-  );
-};
-
-const AboutUser = () => {
-  const { editMode, user } = useSelector((state) => state.ui);
-  const dispatch = useDispatch();
-  const [userInfo, setUserInfo] = useState({
-    role: '',
-    about: '',
-    portfolio_link: '',
-  });
-
-  useEffect(() => {
-    if (editMode) {
-      const { about, fun_fact, portfolio_link, role } = user;
-      setUserInfo({
-        role,
-        about,
-        portfolio_link,
-      });
-    }
-  }, [editMode]);
-
-  const handleUserUpdate = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await updateUser(user._id, userInfo);
-      dispatch(uiActions.updateUser(res));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div className="about-user">
-      <Form formData={createProfileForm} formState={[userInfo, setUserInfo, handleUserUpdate]} />
+    <div className="create-profile">
+      <h3 className="header">Create Profile</h3>
+      <form className="create-profile-form">
+        <div className="input-wrapper">
+          <label htmlFor="occupation">I am a</label>
+          <select className="Occupation-roles">
+            <option selected value="Select occupation">
+              Select Occupation
+            </option>
+            <option value="software-developer">Software Developer</option>
+            <option value="ux-designer">UX Designer</option>
+          </select>
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="about-me">About Me</label>
+          <input
+            required
+            id="about"
+            name="about"
+            type="textarea"
+            onChange={(e) => handleChange(e, 'about-me', setAboutMe)}
+            autoComplete="on"
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="portfolio-link">Portfolio Link</label>
+          <input
+            required
+            id="link"
+            name="link"
+            type="text"
+            onChange={(e) => handleChange(e, 'link', setLink)}
+            autoComplete="on"
+          />
+        </div>
+        {/* <div className="action-btn-container">
+        <SingleActionButton payload={submitFormPayload} />
+      </div> */}
+      </form>
+      
+      <a className="later-link" href="/roulette">
+        Complete later
+      </a>
     </div>
   );
-};
-
-const Form = ({ formData, formState }) => {
-  const { button, handlers, inputs } = formData;
-  const [stateObject, setterFunction, handleSubmit] = formState;
-
-  const submitFormPayload = {
-    handlers: handleSubmit,
-    title: button.text,
-  };
-
-  return (
-    <form className="form" onSubmit={handleSubmit}>
-      {inputs.map((input) => (
-        <div key={input.name} className="input-wrapper">
-          <label htmlFor={input.name}>{parseHtml(input.labelText)}</label>
-          {input.type === 'select' ? (
-            <select
-              defaultValue={input.options[0]}
-              onChange={(e) => handleChange(e, input.name, setterFunction)}
-            >
-              {input.options.map((option) => (
-                <option key={`${input.name}-${option}`} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          ) : (
-            <input
-              id={input.name}
-              name={input.name}
-              onChange={(e) => handleChange(e, input.name, setterFunction)}
-              type={input.type}
-              value={stateObject[input.name]}
-              required={input.required ? true : null}
-            />
-          )}
-        </div>
-      ))}
-      <div className="action-btn-container">
-        <SingleActionButton payload={submitFormPayload} />
-      </div>
-    </form>
-  );
-};
-
-export const createProfileForm = {
-  button: {
-    text: 'Next',
-    type: 'single',
-  },
-  handlers: {
-    // setterFunction: ,
-    // onSubmit: ,
-  },
-  inputs: [
-    {
-      labelText: '<small>I am a</small>',
-      name: 'role',
-      required: true,
-      type: 'select',
-      options: ['Select Occupation', 'UX Designer', 'Software Engineer'],
-    },
-    {
-      labelText: '<small>About Me</small>',
-      name: 'about',
-      max_chars: 250,
-      required: true,
-      type: 'textarea',
-    },
-    {
-      labelText: '<small>Portfolio Link</small>',
-      name: 'portfolio_link',
-      type: 'text',
-    },
-  ],
 };
